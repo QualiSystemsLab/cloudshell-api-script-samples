2cfec98d793dc356be34b7d3a0f74c97cd36e2c6
katzy687 <katzy687@gmail.com>
2022-05-02 13:19:47 +0300

    added get folder resource details

---
+++ automation_api_scripts/get_folder_resource_details.py
@@ -0,0 +1,28 @@
+"""
+iterate over a target folder and print out reserved resources
+"""
+from cloudshell.api.cloudshell_api import CloudShellAPISession
+import json
+
+# add credentials for session
+user = "admin"
+password = "admin"
+server = "localhost"
+domain = "Global"
+
+TARGET_FOLDER = "mocks/DUT"
+# start session
+api = CloudShellAPISession(host=server, username=user, password=password, domain=domain)
+# resources = api.GetFolderContent(fullPath="mocks/DUT", showAllDomains=True)
+contents = api.GetFolderContent(fullPath=TARGET_FOLDER).ContentArray
+
+
+for content in contents:
+    if content.Type == "Resource":
+        root_resource_name = content.Name
+        print(f"root name: {root_resource_name}")
+        reserved_resource = {root_resource_name: []}
+        availability_resources = api.GetResourceAvailability(resourcesNames=[content.Name]).Resources
+        for resource in availability_resources:
+            print(f"Resource: {resource.FullName}, Status: {resource.ReservedStatus}")
+        print("===========")
