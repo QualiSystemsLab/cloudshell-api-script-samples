2a95ee4884b1d6c50fe404b7469c1e7b876f980a
katzy687 <katzy687@gmail.com>
2022-06-29 18:47:08 -0400

    refactored services flow

--- automation_api_scripts/add_connected_services.py
+++ automation_api_scripts/add_connected_services.py
@@ -1,56 +1,65 @@
-import math
 import time
+from typing import List
 
-from cloudshell.api.cloudshell_api import CloudShellAPISession, SetConnectorRequest
-
-
-def chunks(lst, n):
-    """Yield successive n-sized chunks from lst."""
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+from cloudshell.api.cloudshell_api import CloudShellAPISession, SetConnectorRequest, AttributeNameValue, \
+    ResourceDiagramLayoutInfo
+from dataclasses import dataclass, field
 
 
-def add_connected_services(api: CloudShellAPISession, sb_id: str, source_resource_name: str, requested_services: int, service_model: str):
+@dataclass
+class AddServiceRequest:
+    """ attributes are optional - default to empty list """
+    service_model: str
+    service_attrs: List[AttributeNameValue] = field(default_factory=lambda: [])
+
+
+def add_connected_services(api: CloudShellAPISession, sb_id: str, source_resource_name: str,
+                           requested_services_data: List[AddServiceRequest], connected_count: int = 0):
+    """ connected count is to statefully calculate the index for appending services to the matrix """
     col_height = 5
     x_offset = 350
     y_offset = 130
     top_offset = 50
 
-    all_resource_positions = api.GetReservationResourcesPositions(reservationId=sb_id).ResourceDiagramLayouts
-    target_pos_search = [x for x in all_resource_positions if x.ResourceName == source_resource_name]
-    if not target_pos_search:
-        raise ValueError(f"{source_resource_name} not found in position list")
-    target_pos = target_pos_search[0]
+    source_resource_pos = get_resource_position(api, sb_id, source_resource_name)
 
-    connected_services = get_connected_services(api, sb_id, source_resource_name, service_model)
-
-    col_idx = len(connected_services) // col_height
-    row_idx = len(connected_services) % col_height
-    x_pos = target_pos.X + ((col_idx + 1) * x_offset)
+    # init (x,y) coordinates
+    col_idx = connected_count // col_height
+    row_idx = connected_count % col_height
+    x_pos = source_resource_pos.X + ((col_idx + 1) * x_offset)
     y_pos = top_offset + (row_idx * y_offset)
 
-    start_range = len(connected_services) + 1
-    end_range = start_range + requested_services
-    services_to_add = []
-    for i in range(start_range, end_range):
-        curr_name = f"{source_resource_name}--{i}"
+    matrix_idx = connected_count + 1
+    services_to_connect = []
+    for req in requested_services_data:
+        curr_name = f"{source_resource_name}--{matrix_idx}"
+        services_to_connect.append(curr_name)
+
         api.AddServiceToReservation(reservationId=sb_id,
-                                    serviceName=SERVICE_MODEL,
-                                    alias=curr_name)
+                                    serviceName=req.service_model,
+                                    alias=curr_name,
+                                    attributes=req.service_attrs)
         api.SetReservationServicePosition(reservationId=sb_id,
                                           serviceAlias=curr_name,
                                           x=x_pos,
                                           y=y_pos)
-        # time.sleep(3)
-        services_to_add.append(curr_name)
-
-        col_idx = i // col_height
-        row_idx = i % col_height
-        x_pos = target_pos.X + ((col_idx + 1) * x_offset)
+        # update (x,y)
+        col_idx = matrix_idx // col_height
+        row_idx = matrix_idx % col_height
+        x_pos = source_resource_pos.X + ((col_idx + 1) * x_offset)
         y_pos = top_offset + (row_idx * y_offset)
+        matrix_idx += 1
 
-    connect_requests = [SetConnectorRequest(source_resource_name, x, "bi", "") for x in services_to_add]
+    connect_requests = [SetConnectorRequest(source_resource_name, x, "bi", "") for x in services_to_connect]
     api.SetConnectorsInReservation(sb_id, connectors=connect_requests)
+
+
+def get_resource_position(api, sb_id, source_resource_name) -> ResourceDiagramLayoutInfo:
+    all_resource_positions = api.GetReservationResourcesPositions(reservationId=sb_id).ResourceDiagramLayouts
+    target_pos_search = [x for x in all_resource_positions if x.ResourceName == source_resource_name]
+    if not target_pos_search:
+        raise ValueError(f"{source_resource_name} not found in position list")
+    return target_pos_search[0]
 
 
 def get_connected_services(api: CloudShellAPISession, sb_id: str, source_resource: str, service_model: str):
@@ -67,18 +76,22 @@ def get_connected_services(api: CloudShellAPISession, sb_id: str, source_resourc
 
 
 def delete_services(api: CloudShellAPISession, sb_id: str, service_model: str):
+    """ helper to clean up services """
     all_services = api.GetReservationDetails(sb_id, True).ReservationDescription.Services
     target_services = [x for x in all_services if x.ServiceName == service_model]
     api.RemoveServicesFromReservation(sb_id, [x.Alias for x in target_services])
 
 
 if __name__ == "__main__":
-    SANDBOX_ID = "afb76801-f6f0-4281-91e8-53d11c8363f1"
+    SANDBOX_ID = "4f45596e-3c9d-4c11-b9b0-faffb85f67fc"
     TARGET_RESOURCE = "test-linux-host"
     SERVICE_MODEL = "Ginger Agent"
     SERVICE_COUNT = 7
-
     api = CloudShellAPISession(host="localhost", username="admin", password="admin", domain="Global")
-    add_connected_services(api, SANDBOX_ID, TARGET_RESOURCE, SERVICE_COUNT, SERVICE_MODEL)
-    # time.sleep(5)
-    # delete_services(api, SANDBOX_ID, SERVICE_MODEL)
+
+    connected_services = get_connected_services(api, SANDBOX_ID, TARGET_RESOURCE, SERVICE_MODEL)
+
+    service_requests = [AddServiceRequest(SERVICE_MODEL) for x in range(SERVICE_COUNT)]
+    add_connected_services(api, SANDBOX_ID, TARGET_RESOURCE, service_requests, connected_count=len(connected_services))
+    time.sleep(5)
+    delete_services(api, SANDBOX_ID, SERVICE_MODEL)
