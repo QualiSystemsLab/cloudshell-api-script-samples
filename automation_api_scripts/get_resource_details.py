1ebbf164b571c4821fbb9702667e320a2a495c2d
katzy687 <katzy687@gmail.com>
2022-08-30 17:51:15 +0300

    cleanup

--- automation_api_scripts/get_resource_details.py
+++
@@ -1,8 +0,0 @@
-from cloudshell.api.cloudshell_api import CloudShellAPISession
-
-# start session
-api = CloudShellAPISession(host="localhost", username="admin", password="admin", domain="Global")
-
-# find resources in DB - specify models / family / attributes etc
-all_resources = api.FindResources(resourceModel="Putshell").Resources
-print(f"resource count found: {len(all_resources)}")
